3d4d347bf86b48933e078e3b961f0abbab99de52
Travis McLane <tmclane@gmail.com>
2014-08-18 11:34:16 -0500

    implement standalone 'graph'
    * fetch sensor values asynchronously and wait for all results
    * create function to compute date which is 'hours' before the current
      time

--- _attachments/js/graph.js
+++ _attachments/js/graph.js
@@ -1,43 +1,101 @@
-
-
-
 /**
- * Create and draw a line-graph for an individual zone
+ * Create and draw a multi-line graph for an individual zone
  */
+
+var optionalArg = function(args, key, defaultValue) {
+    if(!args[key]) {
+	return defaultValue
+    } else {
+	return args[key]
+    }
+}
+
+var requiredArg = function(args, key) {
+    if(!args[key]) {
+	throw new Error(key + " is required")
+    }
+    else {
+	return args[key]
+    }
+}
+
+
 function ZoneGraph(args) {
     var that = this;
 
+    var containerId = requiredArg(args, 'containerId');
+    var zoneId = requiredArg(args, 'zoneId');
+    var configuration = requiredArg(args, 'configuration');
 
-
-    var optionalVariable = function(args, key, defaultValue) {
-	if(!args[key]) {
-	    return defaultValue
-	} else {
-	    return args[key]
-	}
+    var dateToArray = function(date) {
+        return [date.getFullYear(),
+                date.getMonth() + 1,
+                date.getDate(),
+                date.getHours(),
+                date.getMinutes(),
+                date.getSeconds()]
     }
 
-    var requiredVariable = function(args, key) {
-	if(!args[key]) {
-	    throw new Error(key + " is required")
-	}
-	else {
-	    return args[key]
-	}
+    var endkey = function(sensor_id, end_time) {
+        if (end_time != null) {
+            return "[\"" + sensor_id + "\",[" + dateToArray(end_time) + "]]";
+        }
+        else {
+            return "[\"" + sensor_id + "\",[{}]]";
+        }
     }
 
+    var parseDate = function(Y,M,D,h,m,s, ms) {
+        return new Date(Y, M - 1, D, h, m, s);
+    }
 
+    var query_string = function(sensor_id, start_time, end_time) {
+        return "?startkey=[\"" + sensor_id + "\",[" + dateToArray(start_time).toString() +
+            "]]&endkey=" + endkey(sensor_id, end_time);
+    }
 
+    var fetch_sensor_data = function(sensor_id, start_time, end_time, callback) {
+        d3.json("_view/by_date" + query_string(sensor_id, start_time, end_time),
+                function(viewdata) {
+                    data = viewdata["rows"];
 
+                    var count = 0;
+                    var values = [];
 
-    var containerId = requiredVariable(args, 'containerId');
-    var zoneId = requiredVariable(args, 'zoneId');
+                    data.forEach(function(d) {
+                        d.date = parseDate.apply(null, d.key[1]);
+                        d.temperature = +d.value.F;
+                        count += 1;
+
+                        values.push(d);
+                    });
+
+                    callback(null, {count: count,
+                                    name: sensor_id,
+                                    values: values});
+                });
+    }
+
+    var fetch_sensors = function(sensor_ids, start_time, end_time, callback) {
+        var q = queue(5);
+
+        sensor_ids.forEach(function(s) {
+            q.defer(function(c) { return fetch_sensor_data(s, start_time, end_time, c); });
+        });
+
+        q.awaitAll(function(error, results) {
+            if (error){
+                console.log(error);
+            }
+
+            callback(results);
+        });
+    }
 
     return {
         containerId: containerId,
         zone: zoneId,
-
-        test: function() { console.log('ZoneGraph.test()'); }
-
+        previous_date: function (hours) { return new Date(new Date().getTime() - (hours * 60 * 60 * 1000)); },
+        fetch_sensors: function (start, end) { fetch_sensors(sensorIds, start, end, function(data){ console.log(data);})}
     }
 }
