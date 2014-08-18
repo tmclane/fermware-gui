5023424e897028b41eeded5efe7dcafeab4a0068
Travis McLane <tmclane@gmail.com>
2014-08-18 11:53:28 -0500

    simplify sensor view

--- views/by_date/map.js
+++ views/by_date/map.js
@@ -1,9 +1,8 @@
 function(doc) {
     if(doc.type == 'sensor_value')
         emit([doc.sensor_id, doc.date],
-             {"sensor_id": doc.sensor_id,
-	      "date": doc.date,
-              "C": doc.C,
-              "F": doc.F}
-            );
+             {
+                 "C": doc.C,
+                 "F": doc.F
+             });
 }
